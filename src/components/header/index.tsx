e245b9865419381c8a178e9b83e8daad912878eb
João Victor Vasconcelos <danteopzz@hotmail.com>
2022-11-28 19:56:26 -0300

    Testando

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,14 +1,13 @@
 import { useEffect, useState } from 'react';
 import { AnimatedLetters } from '../animatedLetters';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { 
-    faAngular, 
-    faCss3,
-    faGitAlt,
-    faHtml5, 
-    faJsSquare, 
-    faReact } 
-    from '@fortawesome/free-brands-svg-icons';
+    FaAngular, 
+    FaCss3,
+    FaGitAlt,
+    FaHtml5, 
+    FaJsSquare, 
+    FaReact } 
+    from 'react-icons/fa';
 import './styles.scss';
 
 
@@ -49,22 +48,22 @@ export function Header() {
                 <div className='stage-cube-cont'>
                     <div className='cubespinner'>
                         <div className='face1'>
-                            <FontAwesomeIcon icon={faAngular} color="DD0031" />
+                            <FaAngular color="DD0031" />
                         </div>
                         <div className='face2'>
-                            <FontAwesomeIcon icon={faHtml5} color="F06529" />
+                            <FaHtml5 color="F06529" />
                         </div>
                         <div className='face3'>
-                            <FontAwesomeIcon icon={faCss3} color="28A4D9" />
+                            <FaCss3 color="28A4D9" />
                         </div>
                         <div className='face4'>
-                            <FontAwesomeIcon icon={faReact} color="5ED4F4" />
+                            <FaReact color="5ED4F4" />
                         </div>
                         <div className='face5'>
-                            <FontAwesomeIcon icon={faJsSquare} color="EFD81D" />
+                            <FaJsSquare color="EFD81D" />
                         </div>
                         <div className='face6'>
-                            <FontAwesomeIcon icon={faGitAlt} color="EC4" />
+                            <FaGitAlt color="EC4" />
                         </div>
                     </div>
                 </div>
